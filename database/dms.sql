2f983dae08e76d2e234d5f845b90f70d6e3fafe1
YX310 <2352322148@qq.com>
2022-04-02 21:31:30 +0800

    add batabase
    Signed-off-by: YX310 <2352322148@qq.com>

---
+++ database/dms.sql
@@ -0,0 +1,119 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.5.23 : Database - dms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dms` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `dms`;
+
+/*Table structure for table `defect` */
+
+DROP TABLE IF EXISTS `defect`;
+
+CREATE TABLE `defect` (
+  `defect_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '缺陷id',
+  `defect_name` char(20) NOT NULL COMMENT '缺陷标题',
+  `defect_description` longtext NOT NULL COMMENT '缺陷描述',
+  `priority` char(20) DEFAULT NULL COMMENT '优先级',
+  `probability` varchar(10) DEFAULT NULL COMMENT '概率（几率）',
+  `project_version` char(10) DEFAULT NULL COMMENT '项目版本',
+  `defect_creator` char(10) DEFAULT NULL COMMENT '创建人',
+  `designated_person` char(10) DEFAULT NULL COMMENT '指派人',
+  `defect_module` char(20) DEFAULT NULL COMMENT '缺陷所属模块',
+  `defect_type` char(20) DEFAULT NULL COMMENT '缺陷类型',
+  `start_date` date DEFAULT NULL COMMENT '计划开始日期',
+  `finish_date` date DEFAULT NULL COMMENT '计划完成日期',
+  `progress` char(10) DEFAULT '0' COMMENT '进度',
+  `associated_defects` char(20) DEFAULT NULL COMMENT '关联已有缺陷',
+  `defect_document` char(50) DEFAULT NULL COMMENT '上传的文件',
+  `project_id` int(10) DEFAULT NULL COMMENT '所属项目的id',
+  `defect_state` char(10) DEFAULT NULL COMMENT '缺陷状态',
+  PRIMARY KEY (`defect_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `defect` */
+
+insert  into `defect`(`defect_id`,`defect_name`,`defect_description`,`priority`,`probability`,`project_version`,`defect_creator`,`designated_person`,`defect_module`,`defect_type`,`start_date`,`finish_date`,`progress`,`associated_defects`,`defect_document`,`project_id`,`defect_state`) values (1,'home路径错误','home路径错误','一般','100%','v0.8','cs','xiaoguan','home',NULL,'2022-03-30','2022-03-31','0',NULL,NULL,1,NULL),(3,'defect_test','测试添加缺陷','普通','10%',NULL,NULL,'xiaoguan',NULL,'功能问题',NULL,NULL,NULL,NULL,NULL,NULL,'新建');
+
+/*Table structure for table `project` */
+
+DROP TABLE IF EXISTS `project`;
+
+CREATE TABLE `project` (
+  `project_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '项目id',
+  `project_name` char(20) NOT NULL COMMENT '项目名称',
+  `creator` char(20) NOT NULL COMMENT '创建人',
+  `creation_date` date NOT NULL COMMENT '创建时间',
+  `project_description` char(30) DEFAULT NULL COMMENT '项目描述',
+  `project_member` char(50) DEFAULT NULL COMMENT '项目成员',
+  PRIMARY KEY (`project_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `project` */
+
+insert  into `project`(`project_id`,`project_name`,`creator`,`creation_date`,`project_description`,`project_member`) values (1,'project1','xiaomin','2022-03-25','xiaomin的第一个项目','xiaomin'),(2,'project2','xiaoguan','2022-03-25','xiaoguan的第一个项目','xiaoguan'),(3,'project3','xiaomin','2022-03-25','xiaomin的第二个项目','xiaomin'),(4,'project4','xiaomin','2022-03-28','xiaomin的第三个项目','xiaomin'),(5,'project5','xiaomin','2022-03-28','xiaomin的第四个项目','xiaomin'),(6,'project6','xiaoguan','2022-03-28','xiaoguan的第二个项目','xiaoguan');
+
+/*Table structure for table `project_version` */
+
+DROP TABLE IF EXISTS `project_version`;
+
+CREATE TABLE `project_version` (
+  `id` int(10) NOT NULL COMMENT '版本id',
+  `project_id` int(10) DEFAULT NULL COMMENT '项目id',
+  `version` char(20) DEFAULT NULL COMMENT '版本号',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `project_version` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `username` char(20) NOT NULL COMMENT '用户名（登录时使用）',
+  `nickname` char(20) DEFAULT NULL COMMENT '昵称',
+  `password` char(20) NOT NULL COMMENT '密码',
+  `user_role` char(10) NOT NULL COMMENT '用户角色',
+  `head_img` char(20) DEFAULT NULL COMMENT '头像',
+  `company` char(20) DEFAULT NULL COMMENT '公司',
+  `user_position` char(20) DEFAULT NULL COMMENT '职位',
+  `employee_number` varchar(10) DEFAULT NULL COMMENT '工号',
+  `email` char(20) DEFAULT NULL COMMENT '邮箱',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`user_id`,`username`,`nickname`,`password`,`user_role`,`head_img`,`company`,`user_position`,`employee_number`,`email`) values (2,'xiaomin',NULL,'0000','项目经理',NULL,NULL,'dfdfdfdf','8888','2352322148@qq.com'),(3,'xiaoguan',NULL,'0000','开发',NULL,NULL,NULL,NULL,NULL),(4,'cs',NULL,'0000','测试',NULL,NULL,NULL,NULL,NULL),(5,'lucy','露西','0000','项目经理',NULL,NULL,NULL,NULL,'1111'),(6,'kkk','tom','0000','测试',NULL,NULL,'0000',NULL,'2352322148@qq.com'),(10,'lalala','lalala','0000','项目经理',NULL,NULL,NULL,NULL,'2352322148@qq.com'),(11,'admin','admin','0000','管理员',NULL,NULL,NULL,NULL,'1111');
+
+/*Table structure for table `user_and_project` */
+
+DROP TABLE IF EXISTS `user_and_project`;
+
+CREATE TABLE `user_and_project` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户与项目关系id',
+  `user_id` int(10) NOT NULL COMMENT '用户id',
+  `project_id` int(10) NOT NULL COMMENT '项目id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_and_project` */
+
+insert  into `user_and_project`(`id`,`user_id`,`project_id`) values (1,2,1),(2,2,3),(3,3,2);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

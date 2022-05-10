5fa25ac467e58048b59e5ce537c46bd0dae54349
yuchenzhou <1057433699@qq.com>
2022-05-10 23:33:02 +0800

    fixed back show tips agagin
    Signed-off-by: yuchenzhou <1057433699@qq.com>

--- dms/src/main/resources/static/js/ZeroModalTips.js
+++ dms/src/main/resources/static/js/ZeroModalTips.js
@@ -1,9 +1,7 @@
 /*<![CDATA[*/
 function _showInfo(title, info) {
     console.log(info);
-    console.log(window.performance.navigation.type);
-    console.log(window.performance);
-    if (info != null) {
+    if (window.performance.navigation.type !== 2 && info != null) {
         zeroModal.success({
             content: title,
             contentDetail: info,
